24958721ae86dd4cf3ba5b711dc968bbd32d4d30
xenia marchenko <ksumar4enko@gmail.com>
2022-11-30 00:15:04 +0300

    add more elements in a list of ALL_ITEMS

--- item.js
+++ item.js
@@ -30,5 +30,14 @@ class Item {
 
 let ALL_ITEMS = [
     new Item(['UCLan Hoodie', 'Purple', 'cotton authentic character and practicality are combined in this comfy  warm and luxury hoodie for students that goes with everything to create casual looks', ' Only Ј39.99', 'images/items/hoodies/hoodie (1).jpg']),
-    new Item(['UCLan Hoodie', 'Light Blue', 'cotton authentic character and practicality are combined in this comfy  warm and luxury hoodie for students that goes with everything to create casual looks', ' Only Ј39.99', 'images/items/hoodies/hoodie (2).jpg'])
+    new Item(['UCLan Hoodie', 'Light Blue', 'cotton authentic character and practicality are combined in this comfy  warm and luxury hoodie for students that goes with everything to create casual looks', ' Only Ј39.99', 'images/items/hoodies/hoodie (2).jpg']),
+    new Item(['UCLan Hoodie', 'Green', 'cotton authentic character and practicality are combined in this comfy  warm and luxury hoodie for students that goes with everything to create casual looks', ' Only �39.99', 'images/items/hoodies/hoodie (3).jpg']),
+    new Item(['UCLan Hoodie', 'Green', 'cotton authentic character and practicality are combined in this comfy  warm and luxury hoodie for students that goes with everything to create casual looks', ' Only Ј39.99', 'images/items/hoodies/hoodie (3).jpg']),
+    new Item(['UCLan Hoodie', 'Dark Grey', 'cotton authentic character and practicality are combined in this comfy  warm and luxury hoodie for students that goes with everything to create casual looks', ' Only Ј39.99', 'images/items/hoodies/hoodie (4).jpg']),
+    new Item(['UCLan Hoodie', 'Black', 'cotton authentic character and practicality are combined in this comfy  warm and luxury hoodie for students that goes with everything to create casual looks', ' Only Ј39.99', 'images/items/hoodies/hoodie (5).jpg']),
+    new Item(['UCLan Hoodie', 'Salmon', 'cotton authentic character and practicality are combined in this comfy  warm and luxury hoodie for students that goes with everything to create casual looks', ' Only Ј39.99', 'images/items/hoodies/hoodie (6).jpg']),
+    new Item(['UCLan Hoodie','Burgundy','cotton authentic character and practicality are combined in this comfy  warm and luxury hoodie for students that goes with everything to create casual looks',' Only Ј39.99','images/items/hoodies/hoodie (7).jpg']),
+    new Item(['UCLan Hoodie','Light Grey','cotton authentic character and practicality are combined in this comfy  warm and luxury hoodie for students that goes with everything to create casual looks',' Only Ј39.99','images/items/hoodies/hoodie (8).jpg']),
+    new Item(['UCLan Hoodie','Slate Blue','cotton authentic character and practicality are combined in this comfy  warm and luxury hoodie for students that goes with everything to create casual looks',' Only Ј39.99','images/items/hoodies/hoodie (9).jpg']),
+    new Item(['UCLan Hoodie','Orange','cotton authentic character and practicality are combined in this comfy  warm and luxury hoodie for students that goes with everything to create casual looks',' Only Ј39.99','images/items/hoodies/hoodie (10).jpg'])
 ];
