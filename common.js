0684520b1b1109b6fbf5170b90b13f08c8980fe8
xenia marchenko <ksumar4enko@gmail.com>
2022-12-17 23:54:30 +0300

    font-style and colors

--- common.js
+++ common.js
@@ -2,12 +2,12 @@ function addFooter() {
     let footer = document.createElement("footer");
     footer.innerHTML =
         "<div><h1>Links</h1>" +
-        "<a href=\"https://www.uclansu.co.uk/\">Student's Union</a>" +
+        "<a class=\"footerAndHeaderText\" href=\"https://www.uclansu.co.uk/\">Student's Union</a>" +
         "<h1>Contact</h1>" +
-        "<p>Email: suinformation@uclan.ac.uk</p>" +
-        "<p>Phone: 01772 89 3000</p></div>" +
+        "<p class=\"footerAndHeaderText\">Email: suinformation@uclan.ac.uk</p>" +
+        "<p class=\"footerAndHeaderText\">Phone: 01772 89 3000</p></div>" +
         "<div><h1>Location</h1>" +
-        "<p>University of Central Lancashire Students' Union, " +
+        "<p class=\"footerAndHeaderText\">University of Central Lancashire Students' Union, " +
         "<br>Fylde Road, Preston, PR1 7BY <br>Registered in England " +
         "<br>Company Number: 7623917 " +
         "<br>Registered Charity Number: 1142616</p></div>";
@@ -19,8 +19,8 @@ function addHeader() {
     header.innerHTML =
         "<img src='resources/images/logo.svg' width='200px'>" +
         "<div><h1>Student Shop</h1></div>" +
-        "<div><a href=\"index.html\">Home</a>" +
-        "<a href=\"products.html\">Products</a>" +
-        "<a href=\"cart.html\">Cart</a></div>";
+        "<div><a class=\"footerAndHeaderText\" href=\"index.html\">Home</a>" +
+        "<a class=\"footerAndHeaderText\" href=\"products.html\">Products</a>" +
+        "<a class=\"footerAndHeaderText\" href=\"cart.html\">Cart</a></div>";
     document.body.appendChild(header);
 }
