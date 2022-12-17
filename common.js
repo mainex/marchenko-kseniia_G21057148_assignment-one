3444d9f31a90ee3b7f1614e9459825c4984a5fa7
xenia marchenko <ksumar4enko@gmail.com>
2022-12-18 02:50:05 +0300

    styled home page, header and footer

--- common.js
+++ common.js
@@ -1,12 +1,12 @@
 function addFooter() {
     let footer = document.createElement("footer");
     footer.innerHTML =
-        "<div><h1>Links</h1>" +
-        "<a class=\"footerAndHeaderText\" href=\"https://www.uclansu.co.uk/\">Student's Union</a>" +
-        "<h1>Contact</h1>" +
+        "<div><h1 class=\"footerAndHeaderText\">Links</h1>" +
+        "<a class=\"footerAndHeaderText\" href=\"https://www.uclansu.co.uk/\">Student's Union</a></div>" +
+        "<div style='margin-left: auto;margin-right: auto;'><h1 class=\"footerAndHeaderText\">Contact</h1>" +
         "<p class=\"footerAndHeaderText\">Email: suinformation@uclan.ac.uk</p>" +
         "<p class=\"footerAndHeaderText\">Phone: 01772 89 3000</p></div>" +
-        "<div><h1>Location</h1>" +
+        "<div style='margin-left: auto;margin-right: 50px;'><h1 class=\"footerAndHeaderText\">Location</h1>" +
         "<p class=\"footerAndHeaderText\">University of Central Lancashire Students' Union, " +
         "<br>Fylde Road, Preston, PR1 7BY <br>Registered in England " +
         "<br>Company Number: 7623917 " +
@@ -18,9 +18,11 @@ function addHeader() {
     let header = document.createElement("header");
     header.innerHTML =
         "<img src='resources/images/logo.svg' width='200px'>" +
-        "<div><h1>Student Shop</h1></div>" +
-        "<div><a class=\"footerAndHeaderText\" href=\"index.html\">Home</a>" +
+        "<div><h1 class=\"footerAndHeaderText\">Student Shop</h1></div>" +
+        "<div style=\"margin-right: 10px;margin-left: auto;\">" +
+        "<a class=\"footerAndHeaderText\" href=\"index.html\">Home</a>" +
         "<a class=\"footerAndHeaderText\" href=\"products.html\">Products</a>" +
-        "<a class=\"footerAndHeaderText\" href=\"cart.html\">Cart</a></div>";
+        "<a class=\"footerAndHeaderText\" href=\"cart.html\">Cart</a>" +
+        "</div>";
     document.body.appendChild(header);
 }
