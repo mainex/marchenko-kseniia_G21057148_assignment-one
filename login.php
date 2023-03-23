02b63363f342f654b76f1135ea9413151373df7f
xenia marchenko <ksumar4enko@gmail.com>
2023-03-24 00:15:58 +0300

    login connected with database

--- login.php
+++ login.php
@@ -1,13 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Sign up</title>
-    <link rel="stylesheet" href="shop.css">
-</head>
-<body>
-
-Your email address is: 
-
-</body>
-</html>
+<?php
+$connection = mysqli_connect("vesta.uclan.ac.uk", "kmarchenko", "unvbezEy", "kmarchenko");
+if (mysqli_connect_errno()) {
+    echo "ERROR: could not connect to database: " . mysqli_connect_error();
+}
+$myQuery = "SELECT * FROM tbl_users WHERE user_email='{$_POST["email"]}'";
+$result = mysqli_query($connection, $myQuery);
+while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+    if ($row["user_pass"] === $_POST["password"]) {
+        header("Location: index.html");
+    } else {
+        header("Location: login.html");
+    }
+}
