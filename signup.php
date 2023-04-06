38dffe4d7efe09fa90a9e85d7ad4c125065e80b3
xenia marchenko <ksumar4enko@gmail.com>
2023-04-06 16:31:36 +0300

    prototype of signup submit

--- signup.php
+++ signup.php
@@ -1,2 +1,27 @@
 <?php
-echo $_GET["name"];
+
+if (empty($_POST["fname"])) {
+    die("Full name is required.");
+}
+
+if (!filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
+    die("Valid email is required.");
+}
+if (empty($_POST["password"])) {
+    die("Password is required.");
+}
+if ($_POST["password"] !== $_POST["confirmPassword"]) {
+    die("Passwords must match.");
+}
+if (empty($_POST["address"])) {
+    die("Address is required.");
+}
+
+$passwordHash = password_hash($_POST["password"], PASSWORD_DEFAULT);
+print_r($_POST);
+var_dump($passwordHash);
+
+$connection = mysqli_connect("vesta.uclan.ac.uk", "kmarchenko", "unvbezEy", "kmarchenko");
+if (mysqli_connect_errno()) {
+    echo "ERROR: could not connect to database: " . mysqli_connect_error();
+}
