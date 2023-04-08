39a24fba134f7e13736f852544befbf89245af7f
xenia marchenko <ksumar4enko@gmail.com>
2023-04-09 01:07:15 +0300

    custom 404 error page

---
+++ 404.php
@@ -0,0 +1,21 @@
+<?php
+session_start();
+include_once("common.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>404</title>
+    <link rel="stylesheet" href="shop.css">
+</head>
+
+<body>
+
+<?php addHeader(); ?>
+<h2>Error 404</h2>
+<p style="height: 200px;">Page not found.</p>
+<?php addFooter(); ?>
+
+</body>
+</html>
